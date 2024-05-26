625132eed8f1e57f6fea0a710a87955fbf189604
ADITYAAGAR1221 <128697828+ADITYAAGAR1221@users.noreply.github.com>
2024-05-26 20:57:28 +0530

    making responsive

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -5,6 +5,7 @@ import Offers from '../Components/Offers/Offers'
 import NewCollection from '../Components/NewCollection/NewCollection'
 import NewsLetter from '../Components/NewsLetter/NewsLetter'
 
+
 const Shop = () => {
   return (
     <div>
